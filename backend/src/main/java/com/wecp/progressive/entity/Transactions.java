63b968b9d914d43c83a4b5a61c2977bf7db45b5e
username <wecp-dummy@gmail.com>
2024-01-20 06:04:04 +0000

    wecp initial commit

---
+++ backend/src/main/java/com/wecp/progressive/entity/Transactions.java
@@ -0,0 +1,13 @@
+package com.wecp.progressive.entity;
+import java.util.Date;
+
+
+public class Transactions {
+
+private int transaction_id;
+private int account_id;
+private double amount;
+private Date transaction_date;
+private String transaction_type;
+
+}
