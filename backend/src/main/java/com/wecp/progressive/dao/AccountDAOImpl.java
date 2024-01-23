63ffced703be87207d78c125731938fede39b202
username <wecp-dummy@gmail.com>
2024-01-23 03:47:10 +0000

    wecp initial commit

--- backend/src/main/java/com/wecp/progressive/dao/AccountDAOImpl.java
+++ backend/src/main/java/com/wecp/progressive/dao/AccountDAOImpl.java
@@ -1,5 +1,48 @@
 package com.wecp.progressive.dao;
 
-public class AccountDAOImpl {
+import java.util.*;
 
+import com.wecp.progressive.entity.Accounts;
+
+public class AccountDAOImpl implements AccountDAO {
+
+        ArrayList<Accounts> a1 = new ArrayList<>(); 
+    @Override
+    public int addAccount(Accounts accounts) {
+        // TODO Auto-generated method stub
+        a1.add(accounts);
+        return 1;
+    }
+
+    @Override
+    public void deleteAccount(int accountId) {
+        // TODO Auto-generated method stub
+        for(Accounts a:a1){
+            if(a.getAccount_id()==accountId){
+                a1.remove(a);
+            }
+            
+        }
+
+        
+    }
+
+    @Override
+    public Accounts getAccountById(int accountId) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<Accounts> getAllAccounts() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void updateAccount(Accounts accounts) {
+        // TODO Auto-generated method stub
+        
+    }
+    
 }
