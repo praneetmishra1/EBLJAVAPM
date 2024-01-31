09e78a3e7c42e3140f11152a3285028c11c6ac90
username <wecp-dummy@gmail.com>
2024-01-31 05:02:29 +0000

    wecp initial commit

--- backend/src/main/java/com/wecp/progressive/BankSafeApplication.java
+++ backend/src/main/java/com/wecp/progressive/BankSafeApplication.java
@@ -1,7 +1,10 @@
 package com.wecp.progressive;
 
+import org.springframework.boot.SpringApplication;
+
 public class BankSafeApplication {
     public static void main(String[] args) {
+        SpringApplication.run(BankSafeApplication.class, args);
         System.out.println("Welcome to BankSafe Project!");
     }
 }
